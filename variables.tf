98319059a509f76308c9d866650ed8241dadddc5
Chris Gilmer <chris@truss.works>
2019-06-19 09:17:10 -0700

    Update module to terraform 0.12 syntax

--- variables.tf
+++ variables.tf
@@ -1,26 +1,27 @@
 variable "alb_listener_arn" {
-  type        = "string"
+  type        = string
   description = "(Optional) Associate ACM certificate to and ALB listener."
   default     = ""
 }
 
 variable "domain_name" {
-  type        = "string"
+  type        = string
   description = "Domain name to associate with the ACM certificate."
 }
 
 variable "environment" {
-  type        = "string"
+  type        = string
   description = "Environment tag."
 }
 
 variable "zone_name" {
-  type        = "string"
+  type        = string
   description = "The Route53 zone name for which the certificate should be verified and issued."
 }
 
 variable "caa_records" {
   description = "Add CAA records to route53."
-  type        = "list"
+  type        = list(string)
   default     = []
 }
+
