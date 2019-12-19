67a4438a56e8fa6c6fe5fd6f1e41e0b9535a60e0
Barry Morrison <689591+esacteksab@users.noreply.github.com>
2019-12-19 16:33:59 -0600

    [WIP] Upgrading mater branch to 0.12 syntax

--- main.tf
+++ main.tf
@@ -22,48 +22,49 @@
  */
 
 data "aws_route53_zone" "main" {
-  name = "${var.zone_name}"
+  name = var.zone_name
 }
 
 resource "aws_acm_certificate" "main" {
-  domain_name       = "${var.domain_name}"
+  domain_name       = var.domain_name
   validation_method = "DNS"
 
   tags = {
-    Name        = "${var.domain_name}"
-    Environment = "${var.environment}"
+    Name        = var.domain_name
+    Environment = var.environment
     Automation  = "Terraform"
   }
 }
 
 resource "aws_route53_record" "main" {
-  zone_id = "${data.aws_route53_zone.main.id}"
-  name    = "${aws_acm_certificate.main.domain_validation_options.0.resource_record_name}"
-  type    = "${aws_acm_certificate.main.domain_validation_options.0.resource_record_type}"
-  records = ["${aws_acm_certificate.main.domain_validation_options.0.resource_record_value}"]
+  zone_id = data.aws_route53_zone.main.id
+  name    = aws_acm_certificate.main.domain_validation_options[0].resource_record_name
+  type    = aws_acm_certificate.main.domain_validation_options[0].resource_record_type
+  records = [aws_acm_certificate.main.domain_validation_options[0].resource_record_value]
   ttl     = "60"
 }
 
 resource "aws_acm_certificate_validation" "main" {
-  certificate_arn         = "${aws_acm_certificate.main.arn}"
+  certificate_arn         = aws_acm_certificate.main.arn
   validation_record_fqdns = aws_route53_record.main.*.fqdn
 }
 
 resource "aws_lb_listener_certificate" "main" {
-  count = "${var.alb_listener_arn != "" ? 1 : 0}"
+  count = var.alb_listener_arn != "" ? 1 : 0
 
-  listener_arn    = "${var.alb_listener_arn}"
-  certificate_arn = "${aws_acm_certificate.main.arn}"
+  listener_arn    = var.alb_listener_arn
+  certificate_arn = aws_acm_certificate.main.arn
 
-  depends_on = ["aws_acm_certificate_validation.main"]
+  depends_on = [aws_acm_certificate_validation.main]
 }
 
 # https://docs.aws.amazon.com/acm/latest/userguide/troubleshooting-caa.html
 resource "aws_route53_record" "caa" {
-  count   = "${length(var.caa_records) > 0 ? 1 : 0}"
-  zone_id = "${data.aws_route53_zone.main.id}"
-  name    = "${var.domain_name}"
+  count   = length(var.caa_records) > 0 ? 1 : 0
+  zone_id = data.aws_route53_zone.main.id
+  name    = var.domain_name
   type    = "CAA"
-  records = "${var.caa_records}"
+  records = var.caa_records
   ttl     = "60"
 }
+
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,5 @@
 output "acm_arn" {
   description = "The ARN of the validated ACM certificate."
-  value       = "${aws_acm_certificate_validation.main.certificate_arn}"
+  value       = aws_acm_certificate_validation.main.certificate_arn
 }
+
