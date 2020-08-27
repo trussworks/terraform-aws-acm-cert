979cf22a3410c325af02a6d6127fcdf686378091
ralren <ren@truss.works>
2020-08-27 13:43:39 -0700

    Refactor TF12 code to TF13 code

--- main.tf
+++ main.tf
@@ -18,16 +18,23 @@ resource "aws_acm_certificate" "main" {
 }
 
 resource "aws_route53_record" "main" {
+  for_each = {
+    for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
   zone_id = data.aws_route53_zone.main.id
-  name    = aws_acm_certificate.main.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.main.domain_validation_options[0].resource_record_type
-  records = [aws_acm_certificate.main.domain_validation_options[0].resource_record_value]
   ttl     = "60"
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
 }
 
 resource "aws_acm_certificate_validation" "main" {
   certificate_arn         = aws_acm_certificate.main.arn
-  validation_record_fqdns = aws_route53_record.main.*.fqdn
+  validation_record_fqdns = [for record in aws_route53_record.main : record.fqdn]
 }
 
 resource "aws_lb_listener_certificate" "main" {
